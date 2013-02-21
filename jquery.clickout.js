f4b8178d7a7918e5a0306117e0156798e817c683
Quinton Pike <qrpike@gmail.com>
2013-02-21 15:15:35 -0500

    Updated some styling

--- jquery.clickout.js
+++ jquery.clickout.js
@@ -1,17 +1,16 @@
 
-
 n
 
-  (function( $ ) {
-    $.fn.clickOut = function( callback ) { 
-			var $t 		= this,
-				doc 	= $(document),
-				hide 	= function(event) { 
-				if (!$(event.originalEvent.target).parents('#'+$t.attr('id')).size())
-					callback.call($t);
-				else
-					doc.one("click",hide);
-			};
-			doc.one("click", hide); 
+(function( $ ) {
+	$.fn.clickOut = function( callback ) { 
+		var $t 		= this,
+		doc 		= $(document),
+		hide 		= function(event) { 
+		if (!$(event.originalEvent.target).parents('#'+$t.attr('id')).size())
+			callback.call($t);
+		else
+			doc.one("click",hide);
 		};
-	})( jQuery );
+		doc.one("click", hide); 
+	};
+})( jQuery );
