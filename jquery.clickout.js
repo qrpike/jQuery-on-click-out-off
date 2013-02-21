c09267db064c940187ffcf73f23e63addddeae9b
Quinton Pike <qrpike@gmail.com>
2013-02-21 15:17:08 -0500

    Added licensing

--- jquery.clickout.js
+++ jquery.clickout.js
@@ -1,6 +1,15 @@
-
-n
-
+/*
+ * jQuery On Click Out / Off
+ *
+ * Simplified BSD License (@see License)
+ * @author        Quinton Pike
+ * @copyright     (c) 2013 Quinton Pike
+ * Usage: $(elem).clickOut( callback ) (@see Readme.md)
+ * @version 0.0.1
+ * @requires jQuery
+ */
+ 
+ 
 (function( $ ) {
 	$.fn.clickOut = function( callback ) { 
 		var $t 		= this,
